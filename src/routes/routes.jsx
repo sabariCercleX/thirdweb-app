fbaff39f1e54a63a06574014a4c7369935527da1
sabariCercleX <sabari@cerclex.com>
2023-04-27 08:58:41 +0530

    changes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -16,11 +16,18 @@ const Loadable = (Component) => (props) => {
 };
 
 const MenubarLayout = Loadable(lazy(()=> import('../layouts/MenubarLayout')))
+const Donor = Loadable(lazy(()=> import('../pages/Donor')))
+const CreateDonor = Loadable(lazy(()=> import('../pages/CreateDonor')))
+const BloodRequest = Loadable(lazy(()=> import('../pages/BloodRequest')))
 
 export default function Router() {
     return useRoutes([
+      {path:'', }
     {path:'dashboard', element:<MenubarLayout/>, children:[
-        {path:'loading', element:<LoadingScreen/>}
+        {path:'loading', element:<LoadingScreen/>},
+        {path:'donor', element:<Donor/>},
+        {path:'createdonor', element:<CreateDonor/>},
+        {path:'bloodRequest', element:<BloodRequest/>}
     ]}
   ])
   }
